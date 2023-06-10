688810a3667deb62cfe64e8d69c6ead408bace27
Jimmy <ultish@gmail.com>
2023-06-10 14:56:48 +1000

    eclipse

---
+++ eclipse.bundle/src/eclipse/bundle/EclipsePokemon.java
@@ -0,0 +1,17 @@
+package eclipse.bundle;
+
+import xwm.pokemon.PokemonType;
+
+public class EclipsePokemon implements PokemonType {
+
+	@Override
+	public String name() {
+		return "EclipsePokemon";
+	}
+
+	@Override
+	public int transformations() {
+		return 0;
+	}
+
+}
---
+++ xwm.pokemon.interfaces/src/main/java/xwm/pokemon/PokemonType.java
@@ -0,0 +1,12 @@
+package xwm.pokemon;
+
+/**
+ * Describe the type of Pokemon
+ */
+public interface PokemonType {
+
+    public String name();
+
+    public int transformations();
+
+}
