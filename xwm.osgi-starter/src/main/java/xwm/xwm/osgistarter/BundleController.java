eb54b524c59dd03e3834e7f201e90657f9572f45
Jimmy <ultish@gmail.com>
2023-06-11 11:30:27 +1000

    questions

--- xwm.osgi-starter/src/main/java/xwm/xwm/osgistarter/BundleController.java
+++ xwm.osgi-starter/src/main/java/xwm/xwm/osgistarter/BundleController.java
@@ -6,6 +6,9 @@ import org.osgi.framework.BundleException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.URL;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,15 +37,30 @@ public class BundleController {
          bundle.getBundleId();
 
          try {
-            Class<?> aClass = bundle.loadClass("xwm.pokemon.PokemonType");
-            //            PokemonType a;
+            Class<?> aClass = bundle.loadClass("eclipse.bundle.EclipsePokemon");
 
+            if (aClass != null) {
+               Field[] fields = aClass.getFields();
+               System.out.println(fields);
+               try {
+                  Method name = aClass.getDeclaredMethod("name");
+
+                  bundle.getBundleContext();
+               } catch (NoSuchMethodException e) {
+                  throw new RuntimeException(e);
+               }
+
+            }
             // TODO we need to make a Pokemon extension point
             //  and use those plugins to call an interface.
 
             // TODO we need to load generic bundles that share code for other bundles
             //  we aren't loading the bundles into maven context to call directly from.
             System.out.println(aClass);
+            URL resource = bundle.getResource("eclipse/bundle/EclipsePokemon.class");
+            if (resource != null) {
+
+            }
 
          } catch (ClassNotFoundException e) {
             System.out.println("No Class: " + e.getMessage());
